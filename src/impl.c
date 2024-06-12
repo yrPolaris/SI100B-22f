5e4ead3ec7f79927cbac1466b9c4c5365954bcc1
yrPolaris <q3136873552@163.com>
2024-06-12 15:08:29 +0800

    the first commit

--- src/impl.c
+++ src/impl.c
@@ -1,3 +1,45 @@
+#include <immintrin.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 void impl(int N, int step, double *p) {
-  /* Your code here */
+  double *p_next = (double *)aligned_alloc(32, N * N * sizeof(double));
+  omp_set_num_threads(28);
+  __m256d set = _mm256_set1_pd(0.25);
+  for (int k = 0; k < step; k++) {
+#pragma omp parallel for
+    for (int i = 1; i < N - 1; i++) {
+      int j;
+      for (j = 1; j < N - 4; j += 4) {
+        __m256d above = _mm256_loadu_pd(&p[(i - 1) * N + j]);
+        __m256d below = _mm256_loadu_pd(&p[(i + 1) * N + j]);
+        __m256d right = _mm256_loadu_pd(&p[i * N + j + 1]);
+        __m256d left = _mm256_loadu_pd(&p[i * N + j - 1]);
+        __m256d result = _mm256_add_pd(_mm256_add_pd(above, below),
+                                       _mm256_add_pd(right, left));
+        result = _mm256_mul_pd(result, set);
+        _mm256_storeu_pd(&p_next[i * N + j], result);
+      }
+      for (; j < N - 1; j++) {
+        p_next[i * N + j] = (p[(i - 1) * N + j] + p[(i + 1) * N + j] +
+                             p[i * N + j + 1] + p[i * N + j - 1]) /
+                            4.0;
+      }
+    }
+
+#pragma omp parallel for
+    for (int i = 1; i < N - 1; i++) {
+      int j;
+      for (j = 1; j < N - 4; j += 4) {
+        __m256d next_vals = _mm256_loadu_pd(&p_next[i * N + j]);
+        _mm256_storeu_pd(&p[i * N + j], next_vals);
+      }
+      for (; j < N - 1; j++) {
+        p[i * N + j] = p_next[i * N + j];
+      }
+    }
+  }
+
+  free(p_next);
 }
